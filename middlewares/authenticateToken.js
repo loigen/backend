a12efc761f374924c7664b638ad4180c9f901c37
Dev-Loigen <loigen.lariosa@student.passerellesnumeriques.org>
2024-09-09 23:58:22 -0700

    Update authenticateToken.js

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -2,17 +2,19 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const authenticateToken = (req, res, next) => {
-  const token = req.session.token;
-  console.log("Session token:", token);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Extract the token from the "Bearer" string
+
+  console.log("Token from header:", token);
 
   if (!token) {
-    return res.sendStatus(401);
+    return res.sendStatus(401); // Unauthorized
   }
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) {
       console.log("Token verification failed:", err);
-      return res.sendStatus(403);
+      return res.sendStatus(403); // Forbidden
     }
     console.log("User authenticated:", user);
     req.user = user;
