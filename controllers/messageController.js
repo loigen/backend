4abe1bc1813f3252aa06fcd43bae52cb14e6712c
Dev-Loigen <loigen.lariosa@student.passerellesnumeriques.org>
2024-11-04 19:21:46 -0800

    Update messageController.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,22 +1,17 @@
 const messageModel = require("../schemas/messageModel");
-const CryptoJS = require("crypto-js");
-require("dotenv").config();
-
-const SECRET_KEY = process.env.ENCRYPTION_SECRET_KEY;
 
 const createMessage = async (req, res) => {
   const { chatId, senderId, text } = req.body;
 
-  const encryptedText = CryptoJS.AES.encrypt(text, SECRET_KEY).toString();
-
   const message = new messageModel({
     chatId,
     senderId,
-    text: encryptedText,
+    text,
   });
 
   try {
     const response = await message.save();
+
     res.status(200).json(response);
   } catch (error) {
     console.log(error);
@@ -29,14 +24,7 @@ const getMessages = async (req, res) => {
 
   try {
     const messages = await messageModel.find({ chatId });
-
-    const decryptedMessages = messages.map((message) => {
-      const bytes = CryptoJS.AES.decrypt(message.text, SECRET_KEY);
-      const originalText = bytes.toString(CryptoJS.enc.Utf8);
-      return { ...message._doc, text: originalText };
-    });
-
-    res.status(200).json(decryptedMessages);
+    res.status(200).json(messages);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
