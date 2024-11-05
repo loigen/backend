dace637649cd5bab6e60410e9ee6f39dbd14b976
Dev-Loigen <loigen.lariosa@student.passerellesnumeriques.org>
2024-11-04 21:20:17 -0800

    Update messageController.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,47 +1,17 @@
 const messageModel = require("../schemas/messageModel");
-const crypto = require("crypto");
-
-const secretKey = process.env.SECRET_KEY;
-const iv = process.env.IV;
-
-// Encrypt function
-const encryptText = (text) => {
-  const cipher = crypto.createCipheriv(
-    "aes-256-cbc",
-    Buffer.from(secretKey, "hex"),
-    Buffer.from(iv, "hex")
-  );
-  let encrypted = cipher.update(text, "utf-8", "hex");
-  encrypted += cipher.final("hex");
-  return encrypted;
-};
-
-// Decrypt function
-const decryptText = (encryptedText) => {
-  const decipher = crypto.createDecipheriv(
-    "aes-256-cbc",
-    Buffer.from(secretKey, "hex"),
-    Buffer.from(iv, "hex")
-  );
-  let decrypted = decipher.update(encryptedText, "hex", "utf-8");
-  decrypted += decipher.final("utf-8");
-  return decrypted;
-};
 
 const createMessage = async (req, res) => {
   const { chatId, senderId, text } = req.body;
 
-  // Encrypt the message text before saving
-  const encryptedText = encryptText(text);
-
   const message = new messageModel({
     chatId,
     senderId,
-    text: encryptedText,
+    text,
   });
 
   try {
     const response = await message.save();
+
     res.status(200).json(response);
   } catch (error) {
     console.log(error);
@@ -54,16 +24,7 @@ const getMessages = async (req, res) => {
 
   try {
     const messages = await messageModel.find({ chatId });
-
-    // Decrypt each message's text
-    const decryptedMessages = messages.map((message) => {
-      return {
-        ...message._doc,
-        text: decryptText(message.text),
-      };
-    });
-
-    res.status(200).json(decryptedMessages);
+    res.status(200).json(messages);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
