0e85d403a951947971fe13a6524c6885be07a2d6
Dev-Loigen <loigen.lariosa@student.passerellesnumeriques.org>
2024-11-12 01:25:32 -0800

    Update User.js

--- schemas/User.js
+++ schemas/User.js
@@ -1,28 +1,33 @@
 const mongoose = require("mongoose");
 const { save } = require("node-cron/src/storage");
 
-const userSchema = new mongoose.Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  middleName: { type: String, required: false },
-  Profession: { type: String, required: true },
-  EducationBackground: { type: String, required: true },
-  Religion: { type: String, required: true },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    middleName: { type: String, required: false },
+    Profession: { type: String, required: true },
+    EducationBackground: { type: String, required: true },
+    Religion: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    sex: { type: String, required: true, enum: ["Male", "Female", "other"] },
+    status: { type: String, default: "active" },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    profilePicture: { type: String, default: "" },
+    bio: { type: String, default: "" },
+    birthdate: { type: Date, required: true },
+    otp: { type: String, default: null },
+    otpExpiration: { type: Date, default: null },
   },
-  sex: { type: String, required: true, enum: ["Male", "Female", "other"] },
-  status: { type: String, default: "active" },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" },
-  profilePicture: { type: String, default: "" },
-  bio: { type: String, default: "" },
-  birthdate: { type: Date, required: true },
-  otp: { type: String, default: null },
-  otpExpiration: { type: Date, default: null },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.getGravatarUrl = function () {
   return `https://res.cloudinary.com/dovlzzudf/image/upload/v1723710022/profile_pictures/66a367e4a828e02e834561e0_profile.jpg`;
